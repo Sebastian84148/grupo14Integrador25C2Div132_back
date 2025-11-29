30947d2319714a99122b9b9af84a6e8b75b6fe46
Sebastian Peña <sebastiannpenia@gmail.com>
2025-11-29 02:35:40 -0300

    feat: Agregar endpoint GET /productos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+//========Importaciones==========
 import express from "express";
 import environments from "./api/config/environments.js";
+import connection from "./api/database/db.js";
 
 const app = express();
 const PORT = environments.port;
 
+
+//========Endpoints==========
 app.get("/", (req, res) => {
     res.send("Tecno Store");
 });
+
+app.get("/productos", async (req, res) => {
+    try {
+        const sql = "SELECT * FROM productos";
+        const [rows] = await connection.query(sql);
+        
+        res.status(200).json({
+            payload: rows
+        });
+
+    } catch (error) {
+        console.log("Error obteniendo productos:", error.message);
+
+        res.status(500).json({
+            message: "Error interno al obtener productos"
+        });
+    }
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);

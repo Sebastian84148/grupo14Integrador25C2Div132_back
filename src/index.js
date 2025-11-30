c6fd1c2ab0c6b1659403240929337f03ed7a2c61
Sebastian Peña <sebastiannpenia@gmail.com>
2025-11-29 21:57:37 -0300

    feat: Agregar endpoint GET /productos/:id y habilitar CORS

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@
 import express from "express";
 import environments from "./api/config/environments.js";
 import connection from "./api/database/db.js";
+import cors from "cors";
+
 
 const app = express();
 const PORT = environments.port;
+
+
+
+//========Middlewares==========
+app.use(cors());
 
 
 //========Endpoints==========
@@ -12,6 +19,7 @@ app.get("/", (req, res) => {
     res.send("Tecno Store");
 });
 
+// GET /productos ==>  Enviamos todos los productos.
 app.get("/productos", async (req, res) => {
     try {
         const sql = "SELECT * FROM productos";
@@ -30,6 +38,25 @@ app.get("/productos", async (req, res) => {
     }
 });
 
+// GET /productos/id ==> Enviamos el producto con el id correspondiente.
+app.get("/productos/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const sql = "SELECT * FROM productos WHERE productos.id = ?";
+        const [rows] = await connection.query(sql, [id]);
+
+        res.status(200).json({
+            payload: rows
+        });
+
+    } catch (error) {
+        console.log("Error obteniendo el producto por id:", error.message);
+
+        res.status(500).json({
+            message: "Error interno del servidor"
+        });
+    }
+});
 
 
 app.listen(PORT, () => {
